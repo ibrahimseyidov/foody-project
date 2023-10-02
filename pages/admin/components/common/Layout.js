1f70c72f80443fe642790c002156b88f2e8a6b35
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2023-10-02 16:36:25 +0400

    Fixed Problem

--- pages/admin/components/common/Layout.js
+++ pages/admin/components/common/Layout.js
@@ -4,13 +4,13 @@ import Navbar from '../common/Navbar/Navbar'
 
 const Layout = ({ children }) => {
     return (
-        <body style={{ backgroundColor: "#1E1E30" }}>
+        <div style={{ backgroundColor: "#1E1E30" }}>
             <Header />
             <div className='flex'>
                 <Navbar />
                 {children}
             </div>
-        </body>
+        </div>
     )
 }
 
