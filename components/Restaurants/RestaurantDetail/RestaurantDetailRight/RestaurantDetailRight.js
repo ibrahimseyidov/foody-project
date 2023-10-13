7df9979819406324a486288f1814335c04d14e15
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2023-10-13 23:13:12 +0400

    Created Restaurant Detail Design

---
+++ components/Restaurants/RestaurantDetail/RestaurantDetailRight/RestaurantDetailRight.js
@@ -0,0 +1,163 @@
+import Image from 'next/image'
+import basket from '../../../../assets/icons/basket.svg'
+import pizzas from '../../../../assets/icons/pizzas.svg'
+import delIcon from '../../../../assets/icons/delIcon.svg'
+import styles from './restaurantdetailright.module.css'
+import emptyBasket from '../../../../assets/icons/emptyBasket.png'
+import React from 'react'
+
+const RestaurantDetailRight = () => {
+    return (
+        <>
+            <div className={styles['basket-bg']}>
+                <div className={styles['basket-top']}>
+                    <Image className='mr-2' src={basket} alt='basket' />
+                    <span className={styles['basket-items']}>
+                        3 items
+                    </span>
+                </div>
+                <div className='flex flex-col justify-between'>
+                    <div className={styles['basket-middle']}>
+                        <div className={styles['basket-card']}>
+                            <div className='mr-4'>
+                                <Image src={pizzas} alt='food' />
+                            </div>
+                            <div className={styles['basket-title']}>
+                                <h3>Papa John's Pizza Restaurant</h3>
+                                <span>$15.80</span>
+                            </div>
+                            <div className='flex items-start'>
+                                <div className='flex flex-col bg-white px-2 rounded-xl mr-4'>
+                                    <button>+</button>
+                                    <span className='font-semibold'>2</span>
+                                    <button>-</button>
+                                </div>
+                                <div className='-mt-4'>
+                                    <button>
+                                        <Image src={delIcon} alt='delete' />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['basket-card']}>
+                            <div className='mr-4'>
+                                <Image src={pizzas} alt='food' />
+                            </div>
+                            <div className={styles['basket-title']}>
+                                <h3>Papa John's Pizza Restaurant</h3>
+                                <span>$15.80</span>
+                            </div>
+                            <div className='flex items-start'>
+                                <div className='flex flex-col bg-white px-2 rounded-xl mr-4'>
+                                    <button>+</button>
+                                    <span className='font-semibold'>2</span>
+                                    <button>-</button>
+                                </div>
+                                <div className='-mt-4'>
+                                    <button>
+                                        <Image src={delIcon} alt='delete' />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['basket-card']}>
+                            <div className='mr-4'>
+                                <Image src={pizzas} alt='food' />
+                            </div>
+                            <div className={styles['basket-title']}>
+                                <h3>Papa John's Pizza Restaurant</h3>
+                                <span>$15.80</span>
+                            </div>
+                            <div className='flex items-start'>
+                                <div className='flex flex-col bg-white px-2 rounded-xl mr-4'>
+                                    <button>+</button>
+                                    <span className='font-semibold'>2</span>
+                                    <button>-</button>
+                                </div>
+                                <div className='-mt-4'>
+                                    <button>
+                                        <Image src={delIcon} alt='delete' />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['basket-card']}>
+                            <div className='mr-4'>
+                                <Image src={pizzas} alt='food' />
+                            </div>
+                            <div className={styles['basket-title']}>
+                                <h3>Papa John's Pizza Restaurant</h3>
+                                <span>$15.80</span>
+                            </div>
+                            <div className='flex items-start'>
+                                <div className='flex flex-col bg-white px-2 rounded-xl mr-4'>
+                                    <button>+</button>
+                                    <span className='font-semibold'>2</span>
+                                    <button>-</button>
+                                </div>
+                                <div className='-mt-4'>
+                                    <button>
+                                        <Image src={delIcon} alt='delete' />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['basket-card']}>
+                            <div className='mr-4'>
+                                <Image src={pizzas} alt='food' />
+                            </div>
+                            <div className={styles['basket-title']}>
+                                <h3>Papa John's Pizza Restaurant</h3>
+                                <span>$15.80</span>
+                            </div>
+                            <div className='flex items-start'>
+                                <div className='flex flex-col bg-white px-2 rounded-xl mr-4'>
+                                    <button>+</button>
+                                    <span className='font-semibold'>2</span>
+                                    <button>-</button>
+                                </div>
+                                <div className='-mt-4'>
+                                    <button>
+                                        <Image src={delIcon} alt='delete' />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['basket-card']}>
+                            <div className='mr-4'>
+                                <Image src={pizzas} alt='food' />
+                            </div>
+                            <div className={styles['basket-title']}>
+                                <h3>Papa John's Pizza Restaurant</h3>
+                                <span>$15.80</span>
+                            </div>
+                            <div className='flex items-start'>
+                                <div className='flex flex-col bg-white px-2 rounded-xl mr-4'>
+                                    <button>+</button>
+                                    <span className='font-semibold'>2</span>
+                                    <button>-</button>
+                                </div>
+                                <div className='-mt-4'>
+                                    <button>
+                                        <Image src={delIcon} alt='delete' />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles['basket-checkout']}>
+                        <span className={styles['checkout-text']}>Checkout</span>
+                        <div className={styles['checkout-bg']}>
+                            $37.40
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className='-mt-10'>
+                <Image src={emptyBasket} alt='empty-basket' />
+            </div> */}
+        </>
+    )
+}
+
+export default RestaurantDetailRight
