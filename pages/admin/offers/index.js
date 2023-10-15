0b2752fbb7bef2499a7a4d1e755087ac8001786b
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2023-10-15 22:14:22 +0400

    Created Offers Page Design

--- pages/admin/offers/index.js
+++ pages/admin/offers/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import AdminDashboard from '../index'
+import ProductContainer from '../../../components/Products/ProductsContainer'
+import CategoryType from '../../../components/Products/CategoryType/CategoryType'
+import AdminOffers from '../../../components/AdminOffers/AdminOffers'
 
 const index = () => {
   return (
-    <div>index</div>
+    <>
+      <AdminDashboard>
+        <ProductContainer>
+          <CategoryType pageName={'Offers'} />
+          <AdminOffers />
+        </ProductContainer>
+      </AdminDashboard>
+    </>
   )
 }
 
