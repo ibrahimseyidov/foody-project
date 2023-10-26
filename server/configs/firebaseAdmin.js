30b45bc70126640a5a18265720ff053b6586203b
ibrahimseyidov <ibrahimseyidov72@gmail.com>
2023-10-27 03:54:16 +0400

    Fixed Firebase Config Error

--- server/configs/firebaseAdmin.js
+++ server/configs/firebaseAdmin.js
@@ -2,7 +2,7 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  const serviceAccount = require("./foodyapp-51b6e-firebase-adminsdk-1f971-aef5cf5866.json"); // Replace with your own path
+  const serviceAccount = require("./foody-project-4611e-firebase-adminsdk-7dave-e7ea3895f0.json"); // Replace with your own path
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
